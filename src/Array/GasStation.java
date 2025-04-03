835f23dd484e11493c430e976eb68bc5668cc6a4
Sharib Saifi <www.sharibsaifi.ss@gmail.com>
2025-04-04 00:07:13 +0530

    Gas Station

--- src/Array/GasStation.java
+++ src/Array/GasStation.java
@@ -6,9 +6,36 @@ public class GasStation {
 
   private static void impl(int [] gas, int [] cost){
     System.out.println("Ques: " + Arrays.toString(gas));
+    int totalgas = 0;
+
+    for(int i:gas){
+      totalgas += i;
+    }
+    int totalcost = 0;
+    for(int i:cost){
+      totalcost+=i;
+    }
+    if(totalgas< totalcost){
+      return;
+    }
+    int start=0;
+    int curr=0;
+
+    for(int i=0;i<gas.length;i++){
+      curr += gas[i] - cost[i];
+
+      if(curr < 0){
+        start = i+1;
+        curr = 0;
+      }
+    }
+    System.out.println(start);
+
   }
   public static void main(String[] args) {
     impl(new int[]{1,2,3,4,5}, new int[]{3,4,5,1,2});
+
+
   }
 
 }
