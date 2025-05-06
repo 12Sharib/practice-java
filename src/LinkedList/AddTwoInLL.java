1523d2ff48d942c597c4ffa5a4119a480b357d51
Sharib Saifi <www.sharibsaifi.ss@gmail.com>
2025-05-06 22:58:10 +0530

    AddTwoInLL

--- src/LinkedList/AddTwoInLL.java
+++ src/LinkedList/AddTwoInLL.java
@@ -15,49 +15,41 @@ public class AddTwoInLL {
     linkedList1.addLast(6);
     linkedList1.addLast(4);
 
-
     linkedList.print();
 
     System.out.println();
 
     linkedList1.print();
 
-    LinkedList.Node temp1 = linkedList.head;
-    LinkedList.Node temp2 = linkedList1.head;
+    Node l1 = linkedList.head;
+    Node l2 = linkedList1.head;
 
-    LinkedList.Node node = new Node(-1);
-    LinkedList.Node nodeHead = node;
+    Node node = new Node(-1);
+
+    Node curr = node;
     int carry = 0;
-//   if (l1 == null) return l2;
-//        if (l2 == null) return l1;
-//
-//        ListNode node = new ListNode(-1);
-//        ListNode curr = node;
-//        int carry = 0;
-//
-//        while (l1 != null || l2 != null) {
-//            int sum = carry;
-//
-//            if (l1 != null) {
-//                sum += l1.val;
-//                l1 = l1.next;
-//            }
-//            if (l2 != null) {
-//                sum += l2.val;
-//                l2 = l2.next;
-//            }
-//
-//            curr.next = new ListNode(sum % 10);
-//            carry = sum / 10;
-//
-//            curr = curr.next;
-//        }
-//
-//        if (carry > 0) {
-//            curr.next = new ListNode(carry);
-//        }
-//
-//        return node.next;
+
+    while (l1 != null || l2 != null) {
+      int sum = carry;
+
+      if (l1!=null){
+        sum += l1.data;
+        l1 = l1.next;
+      }
+      if (l2!=null){
+        sum+=l2.data;
+        l2 = l2.next;
+      }
+      curr.next = new Node(sum%10);
+      carry = sum/10;
+
+      curr = curr.next;
+    }
+    if (carry > 0){
+      curr.next = new Node(carry);
+    }
+    System.out.println();
+    linkedList.printUsingList(node.next);
   }
 
 }
