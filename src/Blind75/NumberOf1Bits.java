67b526d6fdde2879f66d729953826e7e669755b3
Sharib Saifi <www.sharibsaifi.ss@gmail.com>
2025-07-13 23:29:52 +0530

    NumberOf1Bits

--- src/Blind75/NumberOf1Bits.java
+++ src/Blind75/NumberOf1Bits.java
@@ -3,10 +3,12 @@ package Blind75;
 public class NumberOf1Bits {
 
   private static void impl(int a) {
+    int res = 0;
     while (a != 0) {
-      System.out.println(a >> 1);
-      a = a >> 1;
+      res += a % 2;
+      a = a / 2;
     }
+    System.out.println(res);
   }
 
   public static void main(String[] args) {
